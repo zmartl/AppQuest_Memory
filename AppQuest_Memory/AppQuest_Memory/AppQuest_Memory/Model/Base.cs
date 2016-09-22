03586d31df80c75f8f887fb557a63213042520c0
zperee <elia.perenzin@bluewin.ch>
2016-09-22 16:21:07 +0200

    Model added

---
+++ AppQuest_Memory/AppQuest_Memory/AppQuest_Memory/Model/Base.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace AppQuest_Memory
+{
+	public class Base
+	{
+		public string Title { get; set; }
+		public string Description { get; set; }
+
+		public Base()
+		{
+		}
+	}
+}
---
+++ AppQuest_Memory/AppQuest_Memory/AppQuest_Memory/Model/StartScreen.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace AppQuest_Memory
+{
+	public class StartScreen : Base
+	{
+		public string Image { get; set; }
+
+		public StartScreen()
+		{
+		}
+	}
+}
